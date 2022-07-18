c9fed6b5ff344678f33005e6699bd9d65beb6121
Anton <antonyefanov@gmail.com>
2022-07-18 12:49:42 +0300

    Disabled blue color on tap (tabets)

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -1,18 +1,30 @@
 import { useContext } from "react";
 import { AppContext } from "../../context";
 
-
 export function Catalog() {
+  const { searchValue, setSearchInput } = useContext(AppContext);
 
-  const { searchValue, setSearchInput } = useContext(AppContext)
-  
   return (
     <div className="catalog">
-      <h1 className="catalog__title">{searchValue ? `Пошук по запиту "${searchValue}"` : 'Каталог товарів'}</h1>
+      <h1 className="catalog__title">
+        {searchValue ? `Пошук по запиту "${searchValue}"` : "Каталог товарів"}
+      </h1>
       <div className="catalog__searchbox">
-        {!searchValue && <img className="catalog__searchbox--icon" src="/e-commerce-ua-merch/img/icons/search.svg" alt="search"/>}
-        <input onChange={setSearchInput} value={searchValue} placeholder="Пошук..." className="catalog__searchbox--field" type="search"/>
-      </div> 
+        {!searchValue && (
+          <img
+            className="catalog__searchbox--icon"
+            src="/e-commerce-ua-merch/img/icons/search.svg"
+            alt="search"
+          />
+        )}
+        <input
+          onChange={setSearchInput}
+          value={searchValue}
+          placeholder="Пошук..."
+          className="catalog__searchbox--field"
+          type="search"
+        />
+      </div>
     </div>
   );
 }
